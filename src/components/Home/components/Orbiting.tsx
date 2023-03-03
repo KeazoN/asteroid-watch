5e85570da06744cc6dca23b44ec4cd9a7db3cd21
Uğur Mercan <keazonoffical@gmail.com>
2023-03-03 22:53:50 +0300

    fix

--- src/components/Home/components/Orbiting.tsx
+++ src/components/Home/components/Orbiting.tsx
@@ -4,13 +4,13 @@ const Orbiting = () => {
   const [nasa, setNasa] = useState(null);
   useEffect(() => {
     fetch(
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-02-26&api_key=${
+      `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${
         import.meta.env.VITE_NASA_API_KEY
       }`
     )
       .then((res) => res.json())
       .then((data) => {
-        setNasa(data.near_earth_objects["2023-02-26"]);
+        setNasa(data.near_earth_objects);
       });
   }, []);
   console.log(nasa);
@@ -25,20 +25,15 @@ const Orbiting = () => {
                 <div
                   className="asteroid"
                   key={index}
-                  style={{
+                  style?={{
                     "--astreoidKM":
                       parseFloat(
-                        data.estimated_diameter.kilometers
-                          .estimated_diameter_max
-                      ) *
-                        5600 +
-                      "px",
+                        data.estimated_diameter.kilometers.estimated_diameter_max
+                      ) * 5600 + "px",
                     "--asteroidSpeed":
                       parseFloat(
                         data.estimated_diameter.feet.estimated_diameter_min
-                      ) *
-                        6000 +
-                      "ms",
+                      ) * 6000 + "ms",
                     "--asteroidFeet":
                       parseFloat(data.absolute_magnitude_h) * 0.5 + "px",
                     "--asteroidLocation":
